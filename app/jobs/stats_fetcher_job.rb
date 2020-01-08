dfe4d8c86d98d1943cad889273c09d02ea74c6a1
Jaiganesh G <jai.g.aux@go-jek.com>
2020-01-08 11:54:12 +0530

    Jai | Add stats fetcher job to fetch stats

--- app/jobs/stats_fetcher_job.rb
+++ app/jobs/stats_fetcher_job.rb
@@ -1,10 +1,11 @@
-class StatsFetcherJob < ApplicationJob
-  def perform(influencer_id)
+class StatsFetcherJob
+  include Sidekiq::Worker
+
+  def perform(influencer)
     influencer_stat = MockStagramClient.new.influencer_stat(influencer)
     DBClient.insert_influencer_stat(influencer_stat)
 
-    StatsFetcherJob.perform_later(influencer_id)
-
-    AvgCalculatorJob.process(influencer_stat)
+    AvgCalculatorJob.perform_async(influencer_stat.as_json)
+    StatsFetcherJob.perform_in(5.minutes, influencer)
   end
 end
