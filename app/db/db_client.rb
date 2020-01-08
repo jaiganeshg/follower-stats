6b0f38cb623d862156f2ccb28028d3b074afbc98
Jaiganesh G <jai.g.aux@go-jek.com>
2020-01-08 11:52:46 +0530

    Jai | Add db client to talk with elastic search

--- app/db/db_client.rb
+++ app/db/db_client.rb
@@ -1,12 +1,12 @@
 class DBClient
-  #URL = ENV , PASSWORD, USERNAME, NO_OF_CONNECTIONS, DB
-
-  def connect
-
+  SERVER_URL = ENV['ELASTICSEARCH_SERVER_URL']
+  INFLUENCER_URL = '/influencer/_doc'
+  def self.insert_influencer_stat(influencer_stat)
+    RestClient.post(self.url(INFLUENCER_URL), influencer_stat.to_json, {content_type: :json, accept: :json})
   end
 
-
-  def insert
-
+  private
+  def self.url(sub_url)
+    SERVER_URL + sub_url
   end
 end
