686a3fa9b2d1b12884cf4cdbace72ba1352d7711
Jaiganesh G <jai.g.aux@go-jek.com>
2020-01-08 11:51:42 +0530

    Jai | Add api clients to talk with mockstagram apis

--- app/api/api_client.rb
+++ app/api/api_client.rb
@@ -1,6 +1,6 @@
 class ApiClient
 
   def is_success(response_code)
-    [200, 201, 202].contains(response_code)
+    [200, 201, 202].include?(response_code)
   end
 end
--- app/api/mock_stagram_client.rb
+++ app/api/mock_stagram_client.rb
@@ -1,20 +1,17 @@
-require 'rest-client'
-require 'json'
-
 class MockStagramClient < ApiClient
-  SERVER_URL = ENV.Read('MOCKSTAGRAM_SERVER_URL')
+  SERVER_URL = ENV['MOCKSTAGRAM_SERVER_URL']
   STAT_API = '/api/v1/influencers/'
 
   def influencer_stat(influencer_id)
-    url = url(STAT_API)
-    influencer_url = url + influencer_id
+    stat_api = url(STAT_API)
+    influencer_url = stat_api + influencer_id.to_s
 
     response = RestClient.get(influencer_url)
     unless is_success(response.code)
       InfluencerStat.new(nil, nil, nil, nil)
     end
       stat = JSON.parse(response.body)
-      InfluencerStat.new(stat[:pk], stat[:username], stat[:followerCount], stat[:followingCount])
+      InfluencerStat.new(stat["pk"], stat["username"], stat["followerCount"], stat["followingCount"])
   end
 
   private
