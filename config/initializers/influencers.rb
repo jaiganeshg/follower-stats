4cdfd89a83d7d2fbe6720b3dd6c7f84d775643c2
Jaiganesh G <jai.g.aux@go-jek.com>
2020-01-08 11:58:49 +0530

    Jai | Add initializer to start the app to fetch stats on server load

--- config/initializers/influencers.rb
+++ config/initializers/influencers.rb
@@ -1,8 +1,8 @@
 class Influencers
-  INFLUENCERS = [1000000, 1000001, 1000002, 100003, 1000004]
-  def track
-    INFLUENCERS.each do
-      StatsFetcherJob
+  INFLUENCERS = [1000000, 1000001, 1000002, 1000003, 1000004]
+  def self.track
+    INFLUENCERS.each do |influencer|
+      StatsFetcherJob.perform_async(influencer)
     end
   end
 end
